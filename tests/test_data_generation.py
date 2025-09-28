4a1b015ad4a324471138a30f5ffadefaa369f8cd
Daniel Antunes <danmca19@gmail.com>
2025-09-28 08:56:35 -0300

    Delete tests directory

--- tests/test_data_generation.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def test_customers_csv_exists():
-    df = pd.read_csv('data/customers.csv')
-    assert 'customer_id' in df.columns
-    assert len(df) > 0
--- tests/test_scd2_merge.sql
+++
@@ -1,2 +0,0 @@
--- Basic test to check if SCD2 merge works
-SELECT COUNT(*) FROM `{project}.{dataset}.dim_customers` WHERE is_current = TRUE;
