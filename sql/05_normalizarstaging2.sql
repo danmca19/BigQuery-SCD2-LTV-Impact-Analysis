f636cf17775e5fda787e6a57cc7c87b9556c58e3
Daniel Antunes <danmca19@gmail.com>
2025-09-29 15:04:35 -0300

    Delete sql directory

--- sql/05_normalizarstaging2.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE TABLE `scd2-ltv-analysis.tracking_risco_cliente.staging_customers` AS
-SELECT
-  CAST(customer_id AS STRING) AS customer_id,
-  name,
-  DATE(signup_date) AS signup_date,
-  SAFE_CAST(risk_segment AS STRING) AS risk_segment,
-  CURRENT_TIMESTAMP() AS load_ts
-FROM `scd2-ltv-analysis.tracking_risco_cliente.customers_rawdata`;
--- sql/11_risk_segment.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-  risk_segment,
-  COUNT(DISTINCT customer_id) AS num_customers,
-  ROUND(AVG(lifetime_value),2) AS avg_ltv,
-  ROUND(SUM(lifetime_value),2) AS total_revenue
-FROM `scd2-ltv-analysis.tracking_risco_cliente.customer_ltv`
-GROUP BY risk_segment
-ORDER BY avg_ltv DESC;
